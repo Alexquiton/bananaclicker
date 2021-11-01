bb6255856d847e81f1131e303e6550a7958dbe07
Alex Quiton <80876135+aquiton@users.noreply.github.com>
2021-11-01 17:06:59 -0400

    Added main screen
    main function added and draw window function

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 #banana clicker
+from typing import Optional
 import pygame
+
+WIDTH, HEIGHT = 900,500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Banana Clicker")
+
+backgroundColor = (255,255,255)
+
+#variables
+playgame = False
+
+def draw_window():
+    WIN.fill(backgroundColor)
+    pygame.display.update()
 
 def main():
 
-    print("running")
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        draw_window()
+
+    
+
+    pygame.quit()
 
 if __name__ == "__main__":
     main()

d9ac8db678631d19609f66920bf61da2438f7932
Alex Quiton <80876135+aquiton@users.noreply.github.com>
2021-11-02 23:23:30 -0400

    Added username and password field boxes

--- main.py
+++ main.py
@@ -6,19 +6,52 @@ WIDTH, HEIGHT = 900,500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Banana Clicker")
 
-backgroundColor = (255,255,255)
 
 #variables
+FPS = 60
+clock = pygame.time.Clock()
 playgame = False
+login = False
+backgroundColor = (255,255,255)
+login_textbox = (214, 214, 214)
 
+#draws up a window with background color of white
 def draw_window():
+    
     WIN.fill(backgroundColor)
+    login_window(login)
     pygame.display.update()
+
+def login_window(logged_in):
+    if(logged_in == False):
+        #in window ask user to either login or register
+        #default is login window with register button
+
+        #dimensions of the text input
+        username_input = pygame.Rect(350,200,140,32)
+        password_input = pygame.Rect(350,240,140,32)
+
+        #drawing the rectangle
+        pygame.draw.rect(WIN,login_textbox,username_input,2)
+        pygame.draw.rect(WIN,login_textbox,password_input,2)
+        pygame.display.flip()
+
+
+
+
+
+
+
+
+
+
+
 
 def main():
 
     run = True
     while run:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
